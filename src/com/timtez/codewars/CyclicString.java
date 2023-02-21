e06f76f8ea9b0b91d07e9c4d5763e60f9385f813
Timothy Olisaeloka Ngonadi <timothyolisaeloka@gmail.com>
2023-02-21 13:59:13 +0100

    added orders codewars question

--- src/com/timtez/codewars/CyclicString.java
+++ src/com/timtez/codewars/CyclicString.java
@@ -2,7 +2,7 @@ package com.timtez.codewars;
 
 public class CyclicString {
     public static void main(String[] args) {
-        int res = cyclicString("cabca");
+        int res = cyclicString("abaca");
         System.out.println("result: " + res);
     }
 
@@ -13,15 +13,8 @@ public class CyclicString {
         // one at i;
         for (int L = 1; L <= n; L++) {
             boolean isCyclic = true;
-            for (int i = L; i < n; i++) {
-                if (s.charAt(i) != s.charAt(i-L)) {
-                    isCyclic = false;
-                    break;
-                }
-            }
-            if (isCyclic) {
-                return L;
-            }
+            for (int i = L; i < n; i++) { if (s.charAt(i) != s.charAt(i-L)) { isCyclic = false; break; } }
+            if (isCyclic) { return L; }
         }
         return n;
     }

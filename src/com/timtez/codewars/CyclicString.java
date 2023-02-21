3ad7a2ee587595281e410c04591c96581dc217ab
Timothy Olisaeloka Ngonadi <timothyolisaeloka@gmail.com>
2023-02-21 03:33:30 +0100

    added cyclic string algo

--- src/com/timtez/codewars/CyclicString.java
+++ src/com/timtez/codewars/CyclicString.java
@@ -1,8 +1,5 @@
 package com.timtez.codewars;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class CyclicString {
     public static void main(String[] args) {
         int res = cyclicString("cabca");
@@ -11,6 +8,9 @@ public class CyclicString {
 
     public static int cyclicString(String s) {
         int n = s.length();
+        // this checks the string for repeating characters, using the inner for loop
+        // to index and search the substring to see if the character at i-l is equal to the
+        // one at i;
         for (int L = 1; L <= n; L++) {
             boolean isCyclic = true;
             for (int i = L; i < n; i++) {

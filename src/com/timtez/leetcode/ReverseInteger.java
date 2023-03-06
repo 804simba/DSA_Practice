224b10fded60d25f970fb8ba0e60c71a97aef62d
Timothy Olisaeloka Ngonadi <timothyolisaeloka@gmail.com>
2023-03-06 07:04:26 +0100

    add new solutions

---
+++ src/com/timtez/leetcode/ReverseInteger.java
@@ -0,0 +1,24 @@
+package com.timtez.leetcode;
+
+public class ReverseInteger {
+    static int reverse(int x) {
+        int finalNumber = 0;
+        while(x!=0) {
+            int lastDigit = x%10;
+            int newNumber = finalNumber * 10 + lastDigit;
+            if (newNumber / 10 != finalNumber) {
+                return 0;
+            }
+            finalNumber = newNumber;
+            x /= 10;
+        }
+        if (finalNumber < Integer.MIN_VALUE || finalNumber > Integer.MAX_VALUE) {
+            return 0;
+        }
+        return finalNumber;
+    }
+    public static void main(String[] args) {
+        int result = reverse(-96424351);
+        System.out.println("Result: " + result);
+    }
+}

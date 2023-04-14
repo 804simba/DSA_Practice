36416a5a256a59438370c88cb4b0ebd4423887ab
timolisa <timothyolisaeloka@gmail.com>
2023-04-14 11:47:55 +0100

    add duplicate numbers

--- src/com/timtez/leetcode/DuplicateNumbers.java
+++ src/com/timtez/leetcode/DuplicateNumbers.java
@@ -6,7 +6,9 @@ import java.util.Map;
 public class DuplicateNumbers {
     public static void main(String[] args) {
         int result = findDuplicate(new int[] {1, 3, 4, 5, 4, 5, 5, 6});
+        int result2 = findDuplicateTwo(new int[] {1, 3, 4, 5, 4, 5, 5, 6});
         System.out.println("result = " + result);
+        System.out.println("result 2 = " + result2);
     }
     public static int findDuplicate(int[] nums) {
         final Map<Integer, Integer> frequencyMap = new HashMap<>();
@@ -20,6 +22,21 @@ public class DuplicateNumbers {
         }
 
         for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                duplicate = entry.getKey();
+            }
+        }
+
+        return duplicate;
+    }
+    public static int findDuplicateTwo(int[] nums) {
+        final Map<Integer, Integer> map = new HashMap<>();
+        int duplicate = 0;
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             if (entry.getValue() > 1) {
                 duplicate = entry.getKey();
             }
